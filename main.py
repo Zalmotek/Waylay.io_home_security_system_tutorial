96d8c9850eea892ea940a68284411c4339b09b39
Constantin Craciun <constantin@zalmotek.com>
2021-08-04 16:32:36 +0300

    update concerning firmware v 1.8.1

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ UsStatus2 = M5Label('Text', x=163, y=127, color=0x000, font=FONT_MONT_14, parent
 image0 = M5Img("res/window.png", x=220, y=37, parent=None)
 image1 = M5Img("res/door.png", x=34, y=37, parent=None)
 image2 = M5Img("res/waylay.png", x=110, y=204, parent=None)
-HttpStatus = M5Label('Text', x=34, y=215, color=0x000, font=FONT_MONT_14, parent=None)
-ps = M5Label('Text', x=238, y=215, color=0x000, font=FONT_MONT_14, parent=None)
+HttpStatus = M5Label('Status', x=34, y=215, color=0x000, font=FONT_MONT_14, parent=None)
+ps = M5Label('Waiting', x=238, y=215, color=0x000, font=FONT_MONT_14, parent=None)
 PirLabel1 = M5Label('PIR:Not Detected', x=9, y=113, color=0x000, font=FONT_MONT_14, parent=None)
 UsStatus1 = M5Label('US:Not Detected', x=193, y=113, color=0x000, font=FONT_MONT_14, parent=None)
 
@@ -36,7 +36,7 @@ def SendPOST():
   global status, json_data, DoorStatus, DataMap
   status = 'No Status'
   try:
-    req = urequests.request(method='POST', url='Fill-in-your-webscript-url',data=json_data, headers={'Content-Type':'application/json'})
+    req = urequests.request(method='POST', url='replace_with_your_webscript_address',data=json_data, headers={'Content-Type':'application/json'})
     ps.set_text_color(0x006600)
     wait(5)
     status = req.status_code
